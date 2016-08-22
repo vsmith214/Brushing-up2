56702dd59f0cf4ce5fa807dcb32d0f76181bfdc7
Scott D'Alessandro <sdaless22@gmail.com>
2016-08-22 16:38:10 -0400

    added new specs

---
+++ 04_symmetric_difference/README.md
@@ -0,0 +1,14 @@
+#Symmetric Difference
+
+Symmetric Difference is the difference between two sets.
+
+So in the Symmetric Difference Bonfire you would work through the arrays of numbers something like this:
+
+```
+symmetricDiff(A, B, C);
+// Translates to:
+
+ symmetricDiff(symmetricDiff(A,B),C);
+ ```
+
+ Review the [following video](https://www.youtube.com/watch?v=PxffSUQRkG4) for an in depth explanation on the Symmetric Difference.
---
+++ 06_event_emitter/event_emitter.js
@@ -0,0 +1,18 @@
+function MyEventEmitter(){
+   this.events = {};
+ }
+
+ MyEventEmitter.prototype.addListener = function(name, cb){
+   if(this.events[name] === undefined){
+     this.events[name] = [cb];
+   } else {
+     this.events[name].push(cb);
+   }
+ };
+
+ MyEventEmitter.prototype.emit = function(name) {
+   var args = [].slice.call(arguments,1);
+   this.events[name].forEach(function(index){
+     index.apply(this, args);
+   });
+ };
